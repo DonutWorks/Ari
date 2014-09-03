ad69cf71511c05f11700b76692d29f508b8faad8
Young Hoon Kim <YoungHoonKim@Youngui-MacBook-Pro.local>
2014-09-03 21:09:20 +0900

    #5 create gate

--- app/controllers/gates_controller.rb
+++ app/controllers/gates_controller.rb
@@ -1,5 +1,19 @@
 class GatesController < ApplicationController
   def new
+    @gate = Gate.new
+
+    
   end
-  
+  def create
+    @gate = Gate.new(gate_params)
+    if @gate.save
+      redirect_to new_gate_path
+    end
+  end
+
+  private
+  def gate_params
+    params.require(:gate).permit(:title, :link, :content, :duedate)
+  end
+
 end
