e09dcee6bc08dc4ab0133e011e3532ff7beeff7f
Hong ChulJu <ang0123dev@gmail.com>
2014-09-04 00:02:12 +0900

    basic auth added

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,9 @@
 class AdminController < ApplicationController
   skip_before_action :authenticate_user!
+  http_basic_authenticate_with name: "habitat", password: "iloveyou"
 
   def index
     
   end
+
 end
