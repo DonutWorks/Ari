f4a02b301f178a985290fded4b3c3be857db3454
Hong ChulJu <ang0123dev@gmail.com>
2014-09-07 13:32:53 +0900

    test cases are refactored with factory_girl not_read_users -> unread_users

---
+++ spec/factories.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do 
+  factory :user do
+    email "John@donutworks.com"
+    username "John"
+    phonenumber "01012341234"
+    major "CS"
+    password "testtest"
+  end
+
+  factory :gate do
+    title "goto google.com"
+    content "google!"
+    link "http://google.com"
+  end
+end
--- spec/models/gate_spec.rb
+++ spec/models/gate_spec.rb
@@ -3,11 +3,8 @@ require "rails_helper"
 RSpec.describe Gate, :type => :model do
   describe "#save" do
     it "should prepend protocol if not exist" do
-      gate = Gate.new
-      gate.link = 'www.google.com'
-      gate.shortenURL = 'www.google.com'
+      gate = create(:gate, link: "www.google.com", shortenURL: "www.google.com")
 
-      expect(gate.save).to eq(true)
       expect(gate.link).to eq('http://www.google.com')
       expect(gate.shortenURL).to eq('http://www.google.com')
     end
@@ -15,7 +12,7 @@ RSpec.describe Gate, :type => :model do
 
   describe "#make_shortenURL" do
     it "should make shorten url" do
-      gate = Gate.new
+      gate = create(:gate)
 
       allow(gate).to receive(:request_to_google) do
         OpenStruct.new({
@@ -31,46 +28,26 @@ RSpec.describe Gate, :type => :model do
   end
 
   describe "#read_users" do
-    before(:each) do
-      @user = User.new
-      @user.email = "test@test.com"
-      @user.username = "John"
-      @user.phonenumber = "01012341234"
-      @user.major = "CS"
-      @user.password = "testtest"
-      @user.save!
-
-      @gate = Gate.new
-      @gate.save!
-    end
-
     it "should return users who read a gate" do
-      expect(@gate.read_users).to eq([])
+      user = create(:user)
+      gate = create(:gate)
 
-      @gate.mark_as_read!(for: @user)
-      expect(@gate.read_users).to eq([@user])
+      expect(gate.read_users).to eq([])
+
+      gate.mark_as_read!(for: user)
+      expect(gate.read_users).to eq([user])
     end
   end
 
-  describe "#not_read_users" do
-    before(:each) do
-      @user = User.new
-      @user.email = "test@test.com"
-      @user.username = "John"
-      @user.phonenumber = "01012341234"
-      @user.major = "CS"
-      @user.password = "testtest"
-      @user.save!
-
-      @gate = Gate.new
-      @gate.save!
-    end
-
+  describe "#unread_users" do
     it "should return users who don't read a gate" do
-      expect(@gate.not_read_users).to eq([@user])
+      user = create(:user)
+      gate = create(:gate)
 
-      @gate.mark_as_read!(for: @user)
-      expect(@gate.not_read_users).to eq([])
+      expect(gate.unread_users).to eq([user])
+
+      gate.mark_as_read!(for: user)
+      expect(gate.unread_users).to eq([])
     end
   end
 end
