fd47d03fbf706e630ad7f9d42cdbcabfb256dc41
minhyeok <minhyeok92@nhnnext.org>
2014-09-09 22:19:50 +0900

    리팩토링

--- app/controllers/admin/export_excel_controller.rb
+++ app/controllers/admin/export_excel_controller.rb
@@ -1,12 +1,10 @@
 class Admin::ExportExcelController < Admin::ApplicationController
-  require 'open-uri'
-  require 'net/http'
 
   def export
     pattern = "이름/기수/전화번호"
 
     url = 'http://club.cyworld.com/club/board/common/CommentList.asp?club_id=52252462&board_type=1&board_no=36&item_seq=157146265'
-    page = Nokogiri::HTML(open(url).read, nil, 'utf-8')
+    page = Nokogiri::HTML(open(url), nil, 'utf-8')
 
     column_name = []
     comments = []
@@ -15,13 +13,12 @@ class Admin::ExportExcelController < Admin::ApplicationController
       column_name.push e.strip
     end
 
-    comments.push column_name
+    page.css('.replylist .obj_rslt').each do |val|
+      comment = {}
 
-    page.css('.replylist .obj_rslt').each do |i|
-      comment = []
 
-      i.text.split('/').each do |e|
-        comment.push e.strip
+      val.text.split('/').each_with_index do |e, i|
+        comment[column_name[i].to_sym] = e
       end
 
       comments.push comment
