a304be6c41b0474851eb13794b059020fec00f72
MujinChae <chaesocool@gmail.com>
2014-09-09 22:33:45 +0900

    export excel controller refactoring

--- app/controllers/admin/export_excel_controller.rb
+++ app/controllers/admin/export_excel_controller.rb
@@ -2,9 +2,16 @@ class Admin::ExportExcelController < Admin::ApplicationController
 
 
   def export
-    pattern = "이름/기수/전화번호"
 
-    url = 'http://club.cyworld.com/club/board/common/CommentList.asp?club_id=52252462&board_type=1&board_no=36&item_seq=157146265'
+
+  end
+
+
+  def export_excel
+
+    pattern = params[:format]
+
+    url = convert_url(params[:notice_link])
     page = Nokogiri::HTML(open(url), nil, 'utf-8')
 
     column_names = []
@@ -29,13 +36,10 @@ class Admin::ExportExcelController < Admin::ApplicationController
       format.html
       format.xls { send_data ExcelExporter.export(comments) }
     end
-
+    
   end
 
-
-  def export_excel
-    format = params[:format]
-    notice_link = params[:notice_link]
-    
+  def convert_url(url)
+    url
   end
 end
