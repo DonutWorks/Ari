a93338b53a350d8f98260e6bb2c575e6224ae89c
Hong ChulJu <ang0123dev@gmail.com>
2014-09-09 23:30:42 +0900

    export_excel_controller is integrated with form_normalizer

--- app/controllers/admin/export_excel_controller.rb
+++ app/controllers/admin/export_excel_controller.rb
@@ -1,20 +1,14 @@
+require 'open-uri'
+
 class Admin::ExportExcelController < Admin::ApplicationController
-
-  require 'open-uri'
-
   def new
-
 
   end
 
-
   def create
-
     pattern = params[:pattern]
-
     url = convert_url(params[:notice_link])
 
-    
     page = Nokogiri::HTML(open(url).read, nil, 'utf-8')
 
     column_names = []
@@ -24,14 +18,11 @@ class Admin::ExportExcelController < Admin::ApplicationController
       column_names.push e.strip
     end
 
-    p column_names.inspect
-
     page.css('.replylist .obj_rslt').each do |val|
       comment = {}
 
-
       val.text.split('/').each_with_index do |e, i|
-        comment[column_names[i].to_sym] = e
+        comment[column_names[i].to_sym] = FormNormalizer.normalize(column_names[i], e)
       end
 
       comments.push comment
@@ -41,9 +32,9 @@ class Admin::ExportExcelController < Admin::ApplicationController
       format.html
       format.xls { send_data ExcelExporter.export(comments) }
     end
-    
   end
 
+private
   def convert_url(url)
     url
   end
