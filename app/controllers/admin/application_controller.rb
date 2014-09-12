1f006a5e6e37e1ff546503dc061e0f61c3be77db
minhyeok <minhyeok92@nhnnext.org>
2014-09-12 17:10:26 +0900

    layout design

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,8 +1,9 @@
 class Admin::ApplicationController < ApplicationController
-
   skip_before_action :authenticate_user!
   http_basic_authenticate_with name: "habitat", password: "iloveyou"
   protect_from_forgery with: :exception
+
+  layout "admin"
 
   def index
     @users = User.all
