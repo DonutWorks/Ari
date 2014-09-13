b08760424befeb4722a554528a97ec6c4f57995d
minhyeok <minhyeok92@nhnnext.org>
2014-09-13 17:33:39 +0900

    #68 welcome#index -> users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  get 'users/show'
+
+  devise_for :users, controllers: { registrations: "users/registrations"}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'users#show', id: 1
+
+  resources :users, only: [:show]
 
   resources :gates, only: [:show]
 
