3d2f0c79793aa8ad2ea4f6e4dcc42b7011717cc0
Hong ChulJu <ang0123dev@gmail.com>
2014-09-15 20:08:06 +0900

    User registration page is moved to admin. (non-admin cannot add users.)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: "users/registrations" }
+  devise_for :users, only: [:session]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
         get 'download_roster_example'
       end
     end
+    resources :user, controller: "users/registrations", only: [:new, :create]
 
     resource :export_excel,  controller: :export_excel,  only: [:new, :create]
   end
