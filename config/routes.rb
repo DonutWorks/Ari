5da69ef3cbf7a0865854076161472e0a2077c615
minhyeok <minhyeok92@nhnnext.org>
2014-09-15 15:32:35 +0900

    라우팅 중복문제 해결

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-  get 'users/index'
-  end
-
   devise_for :users, controllers: { registrations: "users/registrations" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -16,7 +12,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root 'application#index'
 
-    resource :users, only: [:index]
+    resources :users, only: [:index]
     resource :import, controller: :import,  only: [:new, :create]
     resources :gates, only: [:new, :create, :show] do
       collection do
