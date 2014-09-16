02591bb7ab5536adc900d6738654b01d608240aa
minhyeok <minhyeok92@nhnnext.org>
2014-09-17 02:24:38 +0900

    #140 뷰에 무조건전화번호는 짝대기 나오도록

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,9 @@
 module Admin::UsersHelper
+  def pretty_phone_number(phone_number)
+    if phone_number.count("-") != 2
+      phone_number.insert(3, '-').insert(-5, '-')
+    end
+    
+    phone_number
+  end
 end
