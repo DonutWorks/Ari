fef2a99ef537674ab550c683205c8568280eb532
Hong ChulJu <ang0123dev@gmail.com>
2014-09-22 01:36:52 +0900

    renewal admin/gate new, edit view

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -7,4 +7,24 @@ module Admin::ApplicationHelper
       when :alert then "alert alert-danger"
     end
   end
+
+  def validation_state(model_instance, attribute)
+    if model_instance.errors.any?
+      if model_instance.errors[attribute].empty?
+        "has-success"
+      else
+        "has-error"
+      end
+    end
+  end
+
+  def validation_state_feedback(model_instance, attribute)
+    if model_instance.errors.any?
+      if model_instance.errors[attribute].empty?
+        "glyphicon-ok"
+      else
+        "glyphicon-remove"
+      end
+    end
+  end
 end
