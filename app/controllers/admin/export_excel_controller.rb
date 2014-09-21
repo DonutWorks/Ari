a71e695ac338da561bc9bf005e2d555364286b71
minhyeok <minhyeok92@nhnnext.org>
2014-09-21 15:46:55 +0900

    #136 url strip & modify error message

--- app/controllers/admin/export_excel_controller.rb
+++ app/controllers/admin/export_excel_controller.rb
@@ -8,9 +8,9 @@ class Admin::ExportExcelController < Admin::ApplicationController
     pattern = PatternUtil.new(params[:pattern])
 
     begin
-      url = CyworldURL.new(params[:notice_link]).to_comment_view_url
+      url = CyworldURL.new(params[:notice_link].strip).to_comment_view_url
     rescue Exception => e
-      flash[:error] = e.message
+      flash[:error] = "주소가 유효하지 않습니다. 다시 한번 확인해주세요."
       redirect_to new_admin_export_excel_path
       return
     end
