c7f415812e93a7b50cb4db45026130ea0d017026
Hong ChulJu <ang0123dev@gmail.com>
2014-09-23 02:07:40 +0900

    apply rails-i18n

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Ari
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ko
+    config.i18n.available_locales = [:en, :ko]
     config.autoload_paths += %W(#{config.root}/lib)
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
