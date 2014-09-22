4bdccee1adb64c09018a2929c2f0a11e43d9e75e
MujinChae <chaesocool@gmail.com>
2014-09-23 03:24:53 +0900

    refactoring import controller's create

--- app/controllers/admin/import_controller.rb
+++ app/controllers/admin/import_controller.rb
@@ -6,34 +6,14 @@ class Admin::ImportController < Admin::ApplicationController
   end
 
   def create
-    file = Tempfile.new(['data','.xlsx'])
-    file.binmode
-    file.write(params[:upload][:file].read)
-
-    data = Roo::Excelx.new(file.path)
-    data.default_sheet = data.sheets.first
-
-    lastRow = data.last_row
-    lastColumn = data.last_column
+    data = ExcelImporter.import(params[:upload][:file])
 
     @invalid_users = []
     @invalid_ids = []
     normalizer = FormNormalizer.new
 
-    (2..lastRow).each do |i|
-      user = User.new
-
-      user.group_id = normalizer.normalize(data.cell(1, 1), data.cell(i, 1))
-      user.major = data.cell(i, 2)
-      user.student_id = data.cell(i, 3)
-      user.sex = normalizer.normalize(data.cell(1, 4), data.cell(i, 4))
-      user.username = data.cell(i, 5)
-      user.home_phone_number = normalizer.normalize(data.cell(1, 6), data.cell(i, 6))
-      user.phone_number = normalizer.normalize(data.cell(1, 7), data.cell(i, 7))
-      user.emergency_phone_number = normalizer.normalize(data.cell(1, 8), data.cell(i, 8))
-      user.email = normalizer.normalize(data.cell(1, 9), data.cell(i, 9))
-      user.habitat_id = data.cell(i, 10)
-      user.member_type = data.cell(i, 11)
+    (2..data.last_row).each do |i|
+      user = UserModelNormalizer.normalize(normalizer, data, i)
 
       if user.has_invalid_column?
         @invalid_users.push(user)
---
+++ lib/user_model_normalizer.rb
@@ -0,0 +1,21 @@
+require 'roo'
+
+class UserModelNormalizer
+  def self.normalize(normalizer, data, i)
+    user = User.new
+
+    user.group_id = normalizer.normalize(data.cell(1, 1), data.cell(i, 1))
+    user.major = data.cell(i, 2)
+    user.student_id = data.cell(i, 3)
+    user.sex = normalizer.normalize(data.cell(1, 4), data.cell(i, 4))
+    user.username = data.cell(i, 5)
+    user.home_phone_number = normalizer.normalize(data.cell(1, 6), data.cell(i, 6))
+    user.phone_number = normalizer.normalize(data.cell(1, 7), data.cell(i, 7))
+    user.emergency_phone_number = normalizer.normalize(data.cell(1, 8), data.cell(i, 8))
+    user.email = normalizer.normalize(data.cell(1, 9), data.cell(i, 9))
+    user.habitat_id = data.cell(i, 10)
+    user.member_type = data.cell(i, 11)
+
+    user
+  end
+end
