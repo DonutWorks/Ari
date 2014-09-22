d7a10a05f6dcf9617628720b0611983022abbe5d
Hong ChulJu <ang0123dev@gmail.com>
2014-09-22 23:32:16 +0900

    added api keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :kakao, ENV['KAKAO_CLIENT_ID']
+  if Rails.env.production?
+    provider :kakao, "c1fbbaddeb906aad47efc9b59c11eacf"
+  else
+    provider :kakao, "1120cdc5280d18aee720bc31b274b1c4"
+  end
 end
