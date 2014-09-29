7847f65c1b9ec7352d7342ad1fbebaa9cee3fc1b
Hong ChulJu <ang0123dev@gmail.com>
2014-09-29 22:31:55 +0900

    added redirect_url to ticket link

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -28,6 +28,8 @@ class ActivationsController < AuthenticatableController
   end
 
   def show
+    redirect_url = params[:redirect_url]
+
     activator = UserActivator.new
     if activator.activate(params[:code], provider_token)
       flash[:notice] = "카카오톡 인증에 성공하였습니다."
@@ -35,23 +37,20 @@ class ActivationsController < AuthenticatableController
       flash[:error] = "카카오톡 인증에 실패하였습니다."
     end
 
-    # TODO: redirect_to redirect_url (#216)
-    redirect_to session.delete(:return_to) || root_path
+    redirect_to redirect_url || session.delete(:return_to) || root_path
   end
 
 private
   def send_ticket_mail(ticket)
-    verify_url = activation_url(ticket.code)
+    verify_url = URI(activation_url(ticket.code))
+    verify_url.query = "redirect_url=#{session[:return_to]}"
+
     mailgun = Mailgun()
     parameters = {
       :from => "ari@donutworks.com",
       :to => ticket.account_activation.user.email,
       :subject => "서울대 햇빛봉사단의 계정 활성화를 위한 메일입니다.",
-      :html => "<div><h2>서울대 햇빛봉사단 계정을 활성화 시키려면 아래의 링크를 클릭 해주세요.</h2></div><div>#{verify_url}</div>"
-      # :text => <<-BODY
-      #   <h2>계정을 활성화 시키려면 아래의 링크를 클릭 해주세요!</h2> :
-      #   #{verify_url}
-      # BODY
+      :html => "<div><h2>서울대 햇빛봉사단 계정을 활성화 시키려면 아래의 링크를 클릭 해주세요.</h2></div><div>#{verify_url.to_s}</div>"
     }
     mailgun.messages.send_email(parameters)
   end
