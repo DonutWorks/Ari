3ab7b30c445cfc54149d9eaaeca980252b8abbcf
Hong ChulJu <ang0123dev@gmail.com>
2014-09-29 16:28:04 +0900

    login with phone number

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,25 @@ class SessionsController < AuthenticatableController
   skip_before_action :authenticate_user!
 
   def new
+    @user = User.new
+  end
+
+  # log in with phone number
+  def create
+    normalizer = FormNormalizers::PhoneNumberNormalizer.new
+    phone_number = normalizer.normalize(params[:user][:phone_number])
+
+    user = User.find_by_phone_number(phone_number)
+    if user.nil?
+      redirect_to sign_in_users_path
+      return
+    end
+
+    session[:user_id] = user.id
+    redirect_to session.delete(:return_to) || root_path
+
+  rescue FormNormalizers::NormalizeError => e
+    redirect_to sign_in_users_path
   end
 
   def destroy
