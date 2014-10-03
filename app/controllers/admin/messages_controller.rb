470234f77b721859efb4d1d647d1c98ca3bc1d0e
MujinChae <chaesocool@gmail.com>
2014-10-03 23:46:51 +0900

    you can send sms

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -10,14 +10,32 @@ class Admin::MessagesController < Admin::ApplicationController
   def create
     message = Message.new
     message.content = params[:sms_content]
-    
+
     if message.save
       params[:sms_user].each do |user|
-        message_histories = MessageHistory.new
-        message_histories.user_id = user[0]
-        message_histories.message_id = message.id
 
-        message_histories.save
+        user = User.find(user[0])
+
+        sms_info = {
+          from: "114", #보내는 사람
+          to: user.phone_number, #받는 사람
+          user: "donutworks",
+          password: "donutwork1!",
+          text: "[서울대 햇빛봉사단]" + message.content #SMS내용
+        }
+
+        sms_sender = SendSMS.new
+        if sms_sender.send_sms(sms_info)
+
+          message_histories = MessageHistory.new
+          message_histories.user_id = user.id
+          message_histories.message_id = message.id
+
+          message_histories.save
+
+        else
+
+        end
       end
       
       redirect_to admin_message_path(message)
