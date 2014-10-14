b066167843a01aeeba6cb7a97ec4fe67c141a7db
MujinChae <chaesocool@gmail.com>
2014-10-14 17:39:52 +0900

    refactoring queries

--- app/controllers/to_responses_controller.rb
+++ app/controllers/to_responses_controller.rb
@@ -1,14 +1,12 @@
 class ToResponsesController < ApplicationController
   def new
     @notice = Notice.find_by_id(params[:notice_id]) or not_found
-    @notice_to = Response.where(notice: @notice, status: "go")
   end
 
   def create
     @notice = Notice.find_by_id(params[:notice_id]) or not_found
-    @notice_to = Response.where(notice: @notice, status: "go")
 
-    if @notice.to.to_i > @notice_to.count
+    if @notice.to.to_i > @notice.responses.where(status: "go").count
       status = "go"
     else
       status = "wait"
@@ -16,7 +14,8 @@ class ToResponsesController < ApplicationController
 
     response = Response.new(user: current_user, notice: @notice, status: status)
     if response.save
-      flash[:notice] = "응답이 기록되었습니다."
+      flash[:notice] = "참석자로 등록 되었습니다." if status == "go"
+      flash[:notice] = "대기자로 등록 되었습니다." if status == "wait"
     else
       flash[:error] = "잘못된 응답입니다."
     end
