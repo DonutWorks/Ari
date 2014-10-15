353cadc599fd6fca4c9d2e1f60663cdea59b4aa1
Hong ChulJu <ang0123dev@gmail.com>
2014-10-16 00:09:05 +0900

    notices event handler bug fixed. (merging mistake)

--- app/assets/javascripts/admin/notices.js
+++ app/assets/javascripts/admin/notices.js
@@ -17,27 +17,36 @@ $(document).on('ready page:load', function () {
     add_phone_number_to_array();
   });
 
-  function add_phone_number_to_array() {
-    var phone_numbers_cnt = 0;
-
-    $('.sms_check').each(function() {
-      if((this).checked === true) phone_numbers_cnt++;
-    });
-
-    if(phone_numbers_cnt >= 1){
-      $('.notice-full-page').addClass('show-sms');
-      $('.sms-text').slideDown();      
-      $('#phone_numbers').text("총 " + phone_numbers_cnt + "명이 선택 되었습니다.");
-    }
-    else if (phone_numbers_cnt == 0){
-      $('.sms-text').slideUp();
-      $('.notice-full-page').removeClass('show-sms');
-    }
-  }
-
   $('#sms_content').keyup(change_sms_text_size);
 
+  $('.notice-type-option').change(function() {
+    switch($(this).val()){
+      case 'to':
+      $('#to-option').fadeIn('fast');
+      break;
+      default:
+      $('#to-option').fadeOut('fast');
+    }
+  });
 });
+
+function add_phone_number_to_array() {
+  var phone_numbers_cnt = 0;
+
+  $('.sms_check').each(function() {
+    if((this).checked === true) phone_numbers_cnt++;
+  });
+
+  if(phone_numbers_cnt >= 1){
+    $('.notice-full-page').addClass('show-sms');
+    $('.sms-text').slideDown();
+    $('#phone_numbers').text("총 " + phone_numbers_cnt + "명이 선택 되었습니다.");
+  }
+  else if (phone_numbers_cnt == 0){
+    $('.sms-text').slideUp();
+    $('.notice-full-page').removeClass('show-sms');
+  }
+}
 
 function change_sms_text_size(){
   var string = $('#sms_content').val();
@@ -59,16 +68,5 @@ function change_sms_text_size(){
       break;
     }
   }
-  
-  $('#current-text-size').text(utf8length); 
-  
-  $('.notice-type-option').change(function() {
-    switch($(this).val()){
-      case 'to':
-      $('#to-option').fadeIn('fast');
-      break;
-      default:
-      $('#to-option').fadeOut('fast');
-    }
-  });
+  $('#current-text-size').text(utf8length);
 }
