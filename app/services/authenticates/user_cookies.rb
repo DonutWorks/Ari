ffedcac28fa230af9b5df4e87311e12c2b6a2f07
Hong ChulJu <ang0123dev@gmail.com>
2014-10-15 18:56:39 +0900

    authenticates::user_storage is base of authenticates::user_session, authenticates::user_cookies

--- app/services/authenticates/user_cookies.rb
+++ app/services/authenticates/user_cookies.rb
@@ -1,6 +1,6 @@
 module Authenticates
-  class UserCookies
-    [:user_id, :regard_as_activated].each do |key|
+  class UserCookies < UserStorage
+    UserStorage::KEYS.each do |key|
       # cookies getter
       define_method("#{key.to_s}") do
         @cookies.signed[key]
@@ -14,25 +14,6 @@ module Authenticates
 
     def initialize(cookies)
       @cookies = cookies
-    end
-
-    def create!(user, regard_as_activated = false)
-      self.user_id = user.id
-      self.regard_as_activated = regard_as_activated
-      return self
-    end
-
-    def destroy!
-      self.user_id = nil
-      self.regard_as_activated = nil
-      return self
-    end
-
-    def user
-      return @user if @user
-      @user = User.find_by_id(user_id)
-      @user.regard_as_activated = regard_as_activated if @user
-      return @user
     end
   end
 end
--- app/services/authenticates/user_session.rb
+++ app/services/authenticates/user_session.rb
@@ -1,6 +1,6 @@
 module Authenticates
-  class UserSession
-    [:user_id, :regard_as_activated].each do |key|
+  class UserSession < UserStorage
+    UserStorage::KEYS.each do |key|
       # session getter
       define_method("#{key.to_s}") do
         @session[key]
@@ -14,25 +14,6 @@ module Authenticates
 
     def initialize(session)
       @session = session
-    end
-
-    def create!(user, regard_as_activated = false)
-      self.user_id = user.id
-      self.regard_as_activated = regard_as_activated
-      return self
-    end
-
-    def destroy!
-      self.user_id = nil
-      self.regard_as_activated = nil
-      return self
-    end
-
-    def user
-      return @user if @user
-      @user = User.find_by_id(user_id)
-      @user.regard_as_activated = regard_as_activated if @user
-      return @user
     end
   end
 end
