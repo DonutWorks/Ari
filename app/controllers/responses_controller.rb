fdd434ab91bf6abb0794c662a26018d833398429
Hong ChulJu <ang0123dev@gmail.com>
2014-10-17 22:36:04 +0900

    response show page renewal

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -5,18 +5,23 @@ class ResponsesController < ApplicationController
     if current_user.responsed_to?(@notice)
       case @notice.responses.find_by_user_id(current_user).status
       when "yes"
-        @status = "당신은 참가 한다고 대답했습니다."   
+        @status = "참가"
       when "maybe"
-        @status = "당신은 모르겠다고 대답했습니다." 
+        @status = "모름"
       when "no"
-        @status = "당신은 불참 한다고 대답했습니다." 
+        @status = "불참"
       end
     end
   end
 
   def create
     notice = Notice.find_by_id(params[:notice_id]) or not_found
-    response = Response.new(user: current_user, notice: notice, status: params[:status])
+
+    response = Response.find_by_user_id_and_notice_id(current_user.id, notice.id)
+    response = Response.new(user: current_user, notice: notice) unless response
+
+    response.status = params[:status]
+
     if response.save
       flash[:notice] = "응답이 기록되었습니다."
     else
