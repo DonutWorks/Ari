7122a809d55be87ed83af51034af7661468daaa1
Young Hoon Kim <YoungHoonKim@Youngui-MacBook-Pro.local>
2014-10-20 23:44:51 +0900

    Admin can add tags (without autocomplete)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,11 +10,16 @@ class Admin::UsersController < Admin::ApplicationController
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      flash[:notice] = "\"#{@user.username}\"님의 회원 정보 생성에 성공했습니다."
-      redirect_to admin_users_path
-    else
-      render "new"
+
+    @user.transaction do
+      begin
+        associate_user_with_user_tags!
+        @user.save!
+        flash[:notice] = "\"#{@user.username}\"님의 회원 정보 생성에 성공했습니다."
+        redirect_to admin_users_path
+      rescue
+        render "new"
+      end
     end
   end
 
@@ -25,11 +30,15 @@ class Admin::UsersController < Admin::ApplicationController
   def update
     @user = User.find(params[:id])
 
-    if @user.update(user_params)
-      flash[:notice] = "\"#{@user.username}\"님의 회원 정보 수정에 성공했습니다."
-      redirect_to admin_user_path(@user)
-    else
-      render 'edit'
+    @user.transaction do
+      begin
+        associate_user_with_user_tags!
+        @user.update!(user_params)
+        flash[:notice] = "\"#{@user.username}\"님의 회원 정보 수정에 성공했습니다."
+        redirect_to admin_user_path(@user)
+      rescue
+        render 'edit'
+      end
     end
   end
 
@@ -50,4 +59,28 @@ private
   def user_params
     params.require(:user).permit(:username, :email, :phone_number, :major, :student_id, :sex, :home_phone_number, :emergency_phone_number, :habitat_id, :member_type, :generation_id, :birth)
   end
+
+  def associate_user_with_user_tags!
+    referenced_tags = extract_tags(@user, params[:tags])
+
+    @user.tags.destroy_all
+
+    referenced_tags.each do |tag|
+      @user.user_user_tags.build(user_tag_id: tag.id)
+    end if referenced_tags != nil
+  end
+
+  def extract_tags(user, content)
+    pattern = /(\w+);/
+
+    referenced = content.scan(pattern)
+
+    referenced.flatten!.map! do |term|
+
+      UserTag.find_by_tag_name(term)
+
+    end if !referenced.empty?
+
+  end
+
 end
