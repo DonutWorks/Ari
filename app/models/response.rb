f0992bc01e03b54fa4aa601b63a3cb1248615a2e
MujinChae <chaesocool@gmail.com>
2014-10-22 22:22:25 +0900

    sent SMS when user's status is changed

--- app/models/response.rb
+++ app/models/response.rb
@@ -7,6 +7,15 @@ class Response < ActiveRecord::Base
   scope :time, -> (notice) { find_by_notice_id(notice.id).created_at.localtime.strftime("%Y-%m-%d %T") }
   scope :responsed_to_go, -> (notice) { notice.responses.where(status: "go") }
 
+  after_save :send_sms_auto
+
+  def send_sms_auto 
+    comment = "[" + self.notice.title + "] 공지의 참상태가 " + self.status + "로 변경 되었습니다."
+
+    sms_sender = SmsSender.new
+    message = sms_sender.send_message(comment, self.notice, self.user)
+  end
+
   def responsed_at
     created_at.localtime.strftime("%Y-%m-%d %T")
   end
