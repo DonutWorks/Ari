a8cd526802d64c2fafbb632b1197923ee96c09b2
Young Hoon Kim <YoungHoonKim@Youngui-MacBook-Pro.local>
2014-10-23 02:28:36 +0900

    feat: show users' tags in the user list

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -19,4 +19,30 @@ $(document).on('ready page:load', function () {
       },
       index: 1
     }]);
+
+  $('#user_filter_word').textcomplete([
+    {
+      match: /(.*)$/,
+      search: function (term, callback) {
+        $.getJSON('/admin/users/search/' + term)
+        .done(function (res) {
+
+          $("#table-user-list").find("tr:gt(0)").css("display","none");
+
+          for (var i in res) {
+            var user = res[i];
+            $("#tr-user-id-" + user.id).css("display","");
+          }
+
+          callback([]);
+        })
+        .fail(function (res) {
+          callback([]);
+        });
+      },
+      replace: function (user) {
+        return user;
+      },
+      index: 1
+    }]);
 });
