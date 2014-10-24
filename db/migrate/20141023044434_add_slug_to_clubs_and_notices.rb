c8f118bebdab392373c376f1da62d00e8434a64f
Hong ChulJu <ang0123dev@gmail.com>
2014-10-24 17:11:28 +0900

    reindex rollback fixed.

--- db/migrate/20141023044434_add_slug_to_clubs_and_notices.rb
+++ db/migrate/20141023044434_add_slug_to_clubs_and_notices.rb
@@ -2,5 +2,7 @@ class AddSlugToClubsAndNotices < ActiveRecord::Migration
   def change
     add_column :clubs, :slug, :string, unique: true
     add_column :notices, :slug, :string, unique: true
+
+    say "After migration, do this from console: Club.find_each(&:save); Notice.find_each(&:save)"
   end
 end
--- db/migrate/20141024073322_reindex_admin_users_by_email_and_club.rb
+++ db/migrate/20141024073322_reindex_admin_users_by_email_and_club.rb
@@ -1,6 +1,12 @@
 class ReindexAdminUsersByEmailAndClub < ActiveRecord::Migration
-  def change
+  def up
     remove_index :admin_users, :email
     add_index :admin_users, [:email, :club_id], :unique => true
+  end
+
+  def down
+    remove_index :admin_users, [:email, :club_id]
+    # irreversible
+    add_index :admin_users, :email, :unique => true
   end
 end
