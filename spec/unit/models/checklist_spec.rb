94ef27cc0509f4fe6f7581e4c84c6cb5637cf352
김민혁 <minhyeok92@nhnnext.org>
2014-10-25 14:42:08 +0900

    테스트 추가

---
+++ spec/unit/models/checklist_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.describe Checklist, :type => :model do
+  context "when check validations" do
+    before(:each) do
+      @checklist = Checklist.new
+      @checklist.valid?
+    end
+
+    it "should check validation for task" do
+      expect(@checklist.errors.messages.keys).to be_include(:task)
+    end
+
+    it "should check validation for #must_has_assignees" do
+      expect(@checklist.errors.messages.keys).to be_include(:assignees)
+    end
+  end
+end
