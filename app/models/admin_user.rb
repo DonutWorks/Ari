204f892c9f3a69917c8c14b1c0792a71cd7ba3f9
Hong ChulJu <ang0123dev@gmail.com>
2014-10-26 11:54:54 +0900

    remove recoverable

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,7 +2,7 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, #:registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable #:recoverable
 
   validates_presence_of :club_id, :name, :phone_number
 
