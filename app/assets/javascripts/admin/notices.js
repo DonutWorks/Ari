2574336c2719c0827743431ffdbc6beb4e40752f
MujinChae <chaesocool@gmail.com>
2014-10-26 09:35:28 +0900

    _ to - and renaming

--- app/assets/javascripts/admin/notices.js
+++ app/assets/javascripts/admin/notices.js
@@ -91,33 +91,33 @@ function change_sms_text_size(){
 function update_check_status(notice_id, response_id, check, index) {
 
   var url = '/admin/notices/' + notice_id + '/responses/update_check?response_id=' + response_id + "&check=" + check
-  if(check == "memo") url = url + "&memo=" + $('#memo_edit_' + index).val();
+  if(check == "memo") url = url + "&memo=" + $('#memo-edit-' + index).val();
 
   $.getJSON(url)
     .done(function (res) {
       if(check == "absence") {
-        if(res.absence == 1) $('#absence_btn_' + index).addClass("btn-success");
-        else $('#absence_btn_' + index).removeClass("btn-success");
+        if(res.absence == 1) $('#absence-btn-' + index).addClass("btn-success");
+        else $('#absence-btn-' + index).removeClass("btn-success");
       }
       else if(check == "dues") {
-        if(res.dues == 1) $('#dues_btn_' + index).addClass("btn-primary");
-        else $('#dues_btn_' + index).removeClass("btn-primary");
+        if(res.dues == 1) $('#dues-btn-' + index).addClass("btn-primary");
+        else $('#dues-btn-' + index).removeClass("btn-primary");
       }
       else {
         if(res.memo != "") {
-          $('#memo_btn_' + index).addClass("btn-warning");
-          $('#memo_div_' + index).html(res.memo);
+          $('#memo-btn-' + index).addClass("btn-warning");
+          $('#memo-content-' + index).html(res.memo);
 
-          $('#memo_in_' + index).addClass("show_memo").removeClass("hide_memo");
-          $('#memo_not_in_' + index).addClass("hide_memo").removeClass("show_memo");
+          $('#memo-in-' + index).addClass("show-memo").removeClass("hide-memo");
+          $('#memo-not-in-' + index).addClass("hide-memo").removeClass("show-memo");
         }
         else {
-          $('#memo_btn_' + index).removeClass("btn-warning");
+          $('#memo-btn-' + index).removeClass("btn-warning");
 
-          $('#memo_not_in_' + index).addClass("hide_memo").removeClass("show_memo");
-          $('#memo_in_' + index).addClass("hide_memo").removeClass("show_memo");
+          $('#memo-not-in-' + index).addClass("hide-memo").removeClass("show-memo");
+          $('#memo-in-' + index).addClass("hide-memo").removeClass("show-memo");
         }
-        $('#memo_edit_' + index).val("");
+        $('#memo-edit-' + index).val("");
       }
       
     })
@@ -127,14 +127,14 @@ function update_check_status(notice_id, response_id, check, index) {
 }
 
 function edit_memo(notice_id, response_id, check, index) {
-  $('#memo_not_in_' + index).addClass("show_memo").removeClass("hide_memo");
-  $('#memo_in_' + index).addClass("hide_memo").removeClass("show_memo");
+  $('#memo-not-in-' + index).addClass("show-memo").removeClass("hide-memo");
+  $('#memo-in-' + index).addClass("hide-memo").removeClass("show-memo");
 
-  $('#memo_edit_' + index).val($('#memo_div_' + index).html());
+  $('#memo-edit-' + index).val($('#memo-content-' + index).html());
 }
 
 function open_memo(index) {
-  if($('#memo_in_' + index).hasClass("hide_memo")) $('#memo_not_in_' + index).addClass("show_memo").removeClass("hide_memo");
+  if($('#memo-in-' + index).hasClass("hide-memo")) $('#memo-not-in-' + index).addClass("show-memo").removeClass("hide-memo");
 }
 
 
