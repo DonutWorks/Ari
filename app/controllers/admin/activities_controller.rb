7562e13fb444ff7e992a5403f8a98fc633b838b4
MujinChae <chaesocool@gmail.com>
2014-10-28 17:42:11 +0900

    can make new activity

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -1,6 +1,28 @@
 class Admin::ActivitiesController < Admin::ApplicationController
+
+  def new
+    @activity = Activity.new
+  end
+
+  def create
+    @activity = Activity.new(activity_params)
+
+    if @activity.save
+      SlackNotifier.notify("햇빛봉사단 게이트 추가 알림 : #{@activity.title}, #{@activity.description}")
+      redirect_to admin_root_path
+    else
+      render 'new'
+    end
+  end
+
   def index
+    @activities = Activity.all
     @users = User.all
-    @notices = Notice.order('created_at DESC')
+  end
+
+
+private
+  def activity_params
+    params.require(:activity).permit(:title, :description, :event_at)
   end
 end
