e720cda1e1ef35ddf4dbbcb9c4ae3fe58981bc23
김민혁 <minhyeok92@nhnnext.org>
2014-11-04 00:45:03 +0900

    add test

---
+++ spec/unit/lib/bank_account_parser/shinhan_parser_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe BankAccountParser::ShinhanParser do
+  describe "#parse_records" do
+    it "should parse records in file successfully" do
+      file = Roo::Excelx.new("#{Rails.root}/public/account_example_for_test.xlsx")
+      file.default_sheet = file.sheets.first
+      result = BankAccountParser::ShinhanParser.parse_records(file)
+
+      d = Date.parse("2014-10-25")
+      t = Time.parse("18:58:06")
+      dt = DateTime.new(d.year, d.month, d.day, t.hour, t.min, t.sec)
+
+      expect(result).to eq([{
+        record_date: dt,
+        withdraw: "0",
+        deposit: "20000",
+        content: "김민혁"}])
+    end
+  end
+end
