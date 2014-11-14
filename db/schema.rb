d118b2cb10656226e3b2ed50eba12c6429339b3d
Hong ChulJu <ang0123dev@gmail.com>
2014-11-14 15:07:58 +0900

    added a migration to backfill public activities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114032322) do
+ActiveRecord::Schema.define(version: 20141114055612) do
 
   create_table "activities", force: true do |t|
     t.string   "title"
