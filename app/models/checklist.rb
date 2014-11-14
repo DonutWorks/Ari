4c8d7415023932e3d843c52f39ff34fcec05b50c
Hong ChulJu <ang0123dev@gmail.com>
2014-11-14 14:55:54 +0900

    user timeline added.

--- app/models/checklist.rb
+++ app/models/checklist.rb
@@ -1,7 +1,16 @@
 class Checklist < ActiveRecord::Base
+  before_destroy :destroy_public_activities
+
+  include PublicActivity::Model
+  tracked except: :destroy,
+    owner: -> (controller, model_instance) { model_instance.assignees.first },
+    params: {
+      task: :task
+    }
+
   belongs_to :club
   belongs_to :notice
-  has_many :assignee_comments
+  has_many :assignee_comments, dependent: :destroy
   has_many :assign_histories
   has_many :assignees, through: :assign_histories, source: :user
   accepts_nested_attributes_for :assign_histories, reject_if: lambda {|attributes| attributes['user_id'].blank?} 
@@ -13,5 +22,10 @@ class Checklist < ActiveRecord::Base
 private
   def must_have_assignees
     errors.add(:assignees, '수행할 회원을 할당해야 합니다.') if self.assign_histories.empty?
+  end
+
+  def destroy_public_activities
+    activities = PublicActivity::Activity.where(trackable: self)
+    activities.destroy_all
   end
 end
