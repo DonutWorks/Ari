219d5ddf94214dd4ddd015a3573c7a83d7807e46
Hong ChulJu <ang0123dev@gmail.com>
2014-11-16 16:23:20 +0900

    round robin bug fixed

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -12,6 +12,7 @@ class DemoController < ApplicationController
     early_signed_in_club = test_clubs.min_by { |club| club.last_signed_in_at || Date.new(0) }
 
     sign_in(:admin_user, early_signed_in_club.representive)
+    early_signed_in_club.representive.touch(:last_sign_in_at)
     redirect_to club_admin_root_path(early_signed_in_club)
   end
 end
